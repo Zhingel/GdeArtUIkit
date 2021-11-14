635e87419c756030b5ce12c254af5a3461509d75
Stas <staszhingel@icloud.com>
2021-11-14 11:45:24 +0300

    Initial Commit

---
+++ GdeArtUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GdeArtUIkit
+//
+//  Created by Стас Жингель on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
