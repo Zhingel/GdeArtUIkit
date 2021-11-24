02c1cf0a2151cc495dbbd4bcbbaf57df3f6c9d61
Stas <staszhingel@icloud.com>
2021-11-24 12:23:09 +0300

    UIimagePicker

--- GdeArtUIkit/Profile/ProfilePageViewController.swift
+++ GdeArtUIkit/Profile/ProfilePageViewController.swift
@@ -9,16 +9,31 @@ import UIKit
 import FirebaseAuth
 import SwiftUI
 
-class ProfilePageViewController: UIViewController {
-    lazy var contentView: UIHostingController<ProfileSwiftUIView> = {
-        var view = ProfileSwiftUIView()
-        view.loginMenuHandler = { [weak self] in
-            self?.handleAdd()
-        }
-        let vc = UIHostingController(rootView: view)
-        return vc
+class ProfilePageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+//    lazy var contentView: UIHostingController<ProfileSwiftUIView> = {
+//        var view = ProfileSwiftUIView()
+//        view.loginMenuHandler = { [weak self] in
+//            self?.handleAdd()
+//        }
+//        let vc = UIHostingController(rootView: view)
+//        return vc
+//    }()
+    let contentView = UIHostingController(rootView: ProfileSwiftUIView())
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "avatar")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
+        return button
     }()
-    
+    let addOpenCallButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add Open Call", for: .normal)
+        button.layer.borderWidth = 1.6
+        button.layer.borderColor = UIColor(.blue).cgColor
+        button.layer.cornerRadius = 16
+        button.addTarget(self, action: #selector(handleAdd), for: .touchUpInside)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +41,16 @@ class ProfilePageViewController: UIViewController {
         navigationItem.title = user.displayName
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "gear")?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(logOutMethod))
         addChild(contentView)
+        let hadderView = UIView()
+        hadderView.backgroundColor = .white
+        hadderView.addSubview(plusPhotoButton)
+        plusPhotoButton.constraints(top: hadderView.topAnchor, bottom: nil, left: hadderView.leftAnchor, right: nil, paddingTop: 100, paddingBottom: 0, paddingleft: 22, paddingRight: 0, width: 70, height: 70)
+        hadderView.addSubview(addOpenCallButton)
+        addOpenCallButton.constraints(top: hadderView.topAnchor, bottom: nil, left: nil, right: hadderView.rightAnchor, paddingTop: 120, paddingBottom: 0, paddingleft: 0, paddingRight: -12, width: 150, height: 32)
+        view.addSubview(hadderView)
+        hadderView.constraints(top: view.topAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingleft: 0, paddingRight: 0, width: 0, height: 180)
         view.addSubview(contentView.view)
-        contentView.view.constraints(top: view.topAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingleft: 0, paddingRight: 0, width: 0, height: 0)
+        contentView.view.constraints(top: hadderView.bottomAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingleft: 0, paddingRight: 0, width: 0, height: 0)
     }
     override func viewWillAppear(_ animated: Bool) {
 
@@ -63,6 +86,26 @@ class ProfilePageViewController: UIViewController {
         }))
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alertController, animated: true)
+    }
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        else if let originalImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerOriginalImage")] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+        plusPhotoButton.layer.masksToBounds = true
+//        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+//        plusPhotoButton.layer.borderWidth = 1
+        dismiss(animated: true, completion: nil)
+        
     }
 }
 
--- GdeArtUIkit/Profile/ProfileSwiftUIView.swift
+++ GdeArtUIkit/Profile/ProfileSwiftUIView.swift
@@ -10,44 +10,78 @@ import UIKit
 import FirebaseAuth
 
 struct ProfileSwiftUIView: View {
+    let user = Auth.auth().currentUser
+    @State var changeFoto = false
     @ObservedObject var model = art()
     var loginMenuHandler: (() -> Void)?
     var body: some View {
         VStack {
-            HStack {
-                Image("2")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 70, height: 70)
-                    .clipShape(Circle())
-                    .padding(5)
-                    .background(Color.white)
-                    .clipShape(Circle())
-                Spacer()
-                Button {
-                   loginMenuHandler?()
-                } label: {
-                    Text("Add Open Call")
-                        .padding(.vertical, 5)
-                        .padding(.horizontal)
-                        .background(
-                            Capsule()
-                                .stroke(lineWidth: 1.5)
-                        )
-                }
-            }
-            .padding([.leading, .trailing])
+//            HStack {
+//                Button {
+//                    changeFoto.toggle()
+//                } label: {
+//                    Image("avatar")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fill)
+//                        .frame(width: 70, height: 70)
+//                        .clipShape(Circle())
+//                        .padding(5)
+//                        .background(Color.white)
+//                        .clipShape(Circle())
+//                }
+//                Spacer()
+//                Button {
+//                   loginMenuHandler?()
+//                } label: {
+//                    Text("Add Open Call")
+//                        .padding(.vertical, 5)
+//                        .padding(.horizontal)
+//                        .background(
+//                            Capsule()
+//                                .stroke(lineWidth: 1.5)
+//                        )
+//                }
+//            }
+//            .padding([.leading, .trailing])
             Divider()
-            ScrollView {
+            HStack(spacing: 40) {
+                Button {
+                    //
+                } label: {
+                    Image("gear")
+                }
+                Divider().frame(width: 0.5, height: 20)
+                Button {
+                    //
+                } label: {
+                    Image("gear")
+                }
+                Divider().frame(width: 0.5, height: 20)
+                Button {
+                    //
+                } label: {
+                    Image("ribbon")
+                }
+
+            }
+            Divider()
+            ScrollView(showsIndicators: false) {
                 ForEach (model.tasks) { task in
                     if task.post.userUid == Auth.auth().currentUser?.uid {
                         OpenCallCreatorsView(task: task)
                     }
                 }
+                
             }
             .onReceive(NotificationCenter.default.publisher(for: NSNotification.newPost)) { _ in
                 self.model.fetchData()
             }
+        }
+        .actionSheet(isPresented: $changeFoto) {
+            ActionSheet(title: Text("Сменить фото"), buttons: [
+                .default(Text("Открыть галлерею")) { print("fgfgdfd")},
+                .cancel()
+            ])
         }
 
     }
